c4da79fce598a2fb7d7b5ff0364d188cda46ad14
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-12-09 17:09:32 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,32 @@
+import streamlit as st
+st.title("Calculator")
+
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def Multiplication(a,b):
+    return a*b
+
+def Division(a,b):
+    if b==0:
+        return "division by zero not possible"
+    else:
+        return a/b
+    
+operation=st.sidebar.selectbox("Select Operation",("addition",'subtraction','multiplication','division'))
+num1=st.number_input("Enter the first number",value=0)
+num2=st.number_input("Enter teh second number",value=0)
+
+if operation=='addition':
+    result=add(num1,num2)
+elif operation=='subtraction':
+    result=subtract(num1,num2)
+elif operation=='multiplication':
+    result=Multiplication(num1,num2)
+elif operation=='division':
+    result=Division(num1,num2)
+
+st.write(f"Result of {operation}:{result}")
