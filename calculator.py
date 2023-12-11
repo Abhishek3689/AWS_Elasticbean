0bc8ccf8e28019faf98cac50a30435b447872e56
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-12-11 10:34:31 +0530

    python fle updated

--- calculator.py
+++ calculator.py
@@ -19,14 +19,20 @@ def Division(a,b):
 operation=st.sidebar.selectbox("Select Operation",("addition",'subtraction','multiplication','division'))
 num1=st.number_input("Enter the first number",value=0)
 num2=st.number_input("Enter teh second number",value=0)
+def calculator():
+    if st.button('Calculate'):
+        result=0
 
-if operation=='addition':
-    result=add(num1,num2)
-elif operation=='subtraction':
-    result=subtract(num1,num2)
-elif operation=='multiplication':
-    result=Multiplication(num1,num2)
-elif operation=='division':
-    result=Division(num1,num2)
+        if operation=='addition':
+            result=add(num1,num2)
+        elif operation=='subtraction':
+            result=subtract(num1,num2)
+        elif operation=='multiplication':
+            result=Multiplication(num1,num2)
+        elif operation=='division':
+            result=Division(num1,num2)
 
-st.write(f"Result of {operation}:{result}")
+        st.success(f"The result of {operation} is: {result}")
+
+if __name__ == "__main__":
+    calculator()
